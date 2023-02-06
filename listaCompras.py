2af5b7317408adbc6f16f2d280fce52a776b35bd
SaidLeon04 <122801569+SaidLeon04@users.noreply.github.com>
2023-02-06 20:28:39 +0000

    CREAR versión funcional de listaCompras.py RESUBIR numeroAleatorio.py a un repositorio ya conectado a replit.com

--- listaCompras.py
+++ listaCompras.py
@@ -1,18 +1,20 @@
 """
 Sistema para crear una lista de compras
 """
-print ("C O M P R A S\nEscoge una opcion\n\n1. Añadir producto\n2. Finalizar listado\n3. Ver lista\n4. Modificar lista\n5. Eliminar 1 elemento\n6. Eliminar toda la lista")
+print ("C O M P R A S\nEscoge una opcion\n\n1. Añadir producto\n2. Finalizar listado\n3. Ver lista\n4. Modificar lista\n5. Eliminar 1 elemento\n6. Eliminar toda la lista") # menú de opciones
 
 op = int(input("Escoge una opción: "))
 
-compras = []
+compras = [] # array 
 
 while op != 2:
+  # operaciones de la opción 1
     if op == 1:
-        producto = input("Crea tu propia lista de compras\n Producto: ")
+        producto = input("Producto: ")
         compras.append(producto)
-        op = int(input("Escoge una opción "))
+        op = int(input("Escribe una opción para continuar: "))
 
+  # operaciones de la opcion 2
     if op == 3:
         numero_compras = len(compras)
         print ("Numero de productos añadidos: ", numero_compras)
@@ -20,14 +22,19 @@ while op != 2:
           print(i)
         op = int(input("Escoge una opción "))
 
-    """
     if op == 4:
-        print("Escribe el número del elemento para modificar ")
-        modificar = int(input("Escribe: "))
-    """    
+        modificar = int(input("Escribe el número del elemento para modificar,  empezando a contar desde 0\n "))
+        posicion = compras[modificar]
+        nuevo = input("Escribe el nuevo producto: ")
+        compras[modificar] = nuevo
+        print ("Tu lista actual")
+        for i in compras:
+            print(i)
+        op = int(input("Escoge una opción para continuar"))
+     
 
 
-
+  # operaciones de la opción 5
     if op == 5:
         print ("Escribe el número del producto que deseas borrar (ten en cuenta comenzar a contar desde 0)")
         eliminar = int(input("Escribe: "))
@@ -37,7 +44,7 @@ while op != 2:
         op = int(input("Escoge una opción "))
         
       
-
+  # operaciones de la opción 6
     if op == 6:
         print ("Eliminar toda la lista y los productos añadidos, ¿estas seguro?\n1. SI || 2. NO\n")
         read = int(input())
@@ -49,7 +56,7 @@ while op != 2:
             print ("Estuviste cerca de borrar tu lista D:")
             op = int(input("Escoge una opción "))
 
-
+# operaciones de la opción 2
 if op ==2:
     print ("Tu lista de compras: ")
     for i in compras:
