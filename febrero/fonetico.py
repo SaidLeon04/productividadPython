c3efe5616562370bfcff5fab2181bf7415cbbcc3
Said León <122801569+SaidLeon04@users.noreply.github.com>
2023-02-25 22:05:28 -0600

    ACTUALIZACION fonetico.py
    ciclo while colgado con 2 o mas caracteres

--- febrero/fonetico.py
+++ febrero/fonetico.py
@@ -5,4 +5,10 @@ while i<len(word):
     print (word[i])
     if word[i] == "a" or word[i] == "A":
         print("Alpha")
-    i =+ 1
+        i =+ 1
+    if word[i] == "b" or word[i] == "B":
+        print("Bravo")
+        i =+ 1
+    if word[i] == "c" or word[i] == "C":
+        print("Charlie")
+        i =+ 1
