8d3a6072bbb92bd0c4eddedb091f89362c15c151
Said León <122801569+SaidLeon04@users.noreply.github.com>
2023-02-26 19:01:28 -0600

    ACTUALIZACION de fonetico.py
    Actualización funcional con todo el alfabeto y el espacio nulo

--- febrero/fonetico.py
+++ febrero/fonetico.py
@@ -1,14 +1,59 @@
-word = input("Escribe una palabra ")
-i = 0
+word = input("Escribe una palabra: ")
 
-while i<len(word):
-    print (word[i])
-    if word[i] == "a" or word[i] == "A":
+for i in word:
+    #print (i)
+    if i == "a" or i == "A":
         print("Alpha")
-        i =+ 1
-    if word[i] == "b" or word[i] == "B":
+    if i == "b" or i == "B":
         print("Bravo")
-        i =+ 1
-    if word[i] == "c" or word[i] == "C":
+    if i == "c" or i == "C":
         print("Charlie")
-        i =+ 1
+    if i == "d" or i == "D":
+        print("Delta")
+    if i == "E" or i == "e":
+        print("Echo")
+    if i == "F" or i == "f":
+        print("Foxtrot")
+    if i == "g" or i == "G":
+        print("Golf")
+    if i == "h" or i == "H":
+        print("Hotel")
+    if i == "I" or i == "i":
+        print("India")
+    if i == "J" or i == "j":
+        print("Juliet")
+    if i == "K" or i == "k":
+        print("Kilo")
+    if i == "L" or i == "l":
+        print("Lima")
+    if i == "M" or i == "m":
+        print("Mike")
+    if i == "N" or i == "n":
+        print("November")
+    if i == "O" or i == "o":
+        print("Oscar")
+    if i == "p" or i == "P":
+        print("Papa")
+    if i == "Q" or i == "q":
+        print("Quebec")
+    if i == "R" or i == "r":
+        print("Romeo")
+    if i == "S" or i == "s":
+        print("Sierra")
+    if i == "t" or i == "T":
+        print("Tango")
+    if i == "u" or i == "U":
+        print("Uniform")
+    if i == "V" or i == "v":
+        print("Victor")
+    if i == "w" or i == "W":
+        print("Whiskey")
+    if i == "x" or i == "X":
+        print("X-ray")
+    if i == "y" or i == "Y":
+        print("Yankee")
+    if i == "z" or i == "Z":
+        print("Zulu")
+    if i == " " or i == " ":
+        print(" ")
+    
