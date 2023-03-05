12b6077ee0ab5e735231f6e9464a0e9400fdb655
Said León <122801569+SaidLeon04@users.noreply.github.com>
2023-03-04 20:25:05 -0600

    INTRODUCIR los valores por teclado

--- marzo/prueba.py
+++ marzo/prueba.py
@@ -3,16 +3,14 @@ from openpyxl import Workbook
 Crea un libro de excel con dos hojas, añade texto a diferentes celdas en ambas hojas
 
 def crear_libro(path):
+	ide = input("Escribe el ID: ")
 	workbook = Workbook() # crea un objeto de la clase woorkbook
 	sheet = workbook.active # crea la primera hoja
 	sheet.title = "productos" " # cambia el nombre de la hoja
 	sheet2 = workbook.create_sheet(title =  "tiendas") # crea una segunda hoja y le coloca el nombre en la misma linea
 
 # añade texto a diferentes celdas referenciadas
-	sheet["A1"] = "Se viene cositas"
-	sheet["A2"] = "Se viene cositas"
-	sheet2["A1"] = "Se viene cositas"
-	sheet2["A3"] = "Se viene cositas"
+	sheet["A1"] = ide
 	workbook.save(path) 3 guarda los cambios en el libro
 	print (workbook.sheetnames) # imprime el nombre de las hojas ya creadas, solo para comprobar
 
